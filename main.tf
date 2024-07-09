5463f7017203a74e1d37e7ef97e1cbfc16f523f8
Neha Nilajkar <56997283+Nehanilajkar@users.noreply.github.com>
2024-07-09 12:59:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,39 @@
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name = var.key
+  vpc_security_group_ids = [aws_security_group.Jenkins_SG.id]
+  user_data = 
 
   tags = {
     Name = var.name
+  }
+}
+
+resource "aws_security_group" "Jenkins_SG" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = var.sg_name
+  }
+
+  ingress {
+   for port in [22,80,443,8080,9000]: 
+    {
+      description = "Inbound rule"
+      from_port = port
+      to_port = port
+      protocol = "tcp"
+    }
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
