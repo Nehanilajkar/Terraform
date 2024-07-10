412750066b6613aad849052992f07f81d2773024
Neha Nilajkar <56997283+Nehanilajkar@users.noreply.github.com>
2024-07-10 12:44:42 +0530

    added ingress rule for security group

--- main.tf
+++ main.tf
@@ -26,6 +26,11 @@ resource "aws_security_group" "Jenkins_SG" {
       from_port = port
       to_port = port
       protocol = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
   ]
 
